d1187a5e4cc057d2ae1949652d226dec1a82e38a
Sebastiao Zoio <szoio@hotmail.com>
2023-10-16 16:04:17 +0100

    add images

--- application.py
+++ application.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():    
-    return render_template('message.html', string="HOME")
+    return render_template('message.html', string="test_api")
 
 @app.route('/test_api', methods=['GET'])
 def test_api():
     
-    return render_template('message.html', string="GET")
+    return render_template('hello.html', name = "World!")
 
 if __name__ == '__main__':
 
